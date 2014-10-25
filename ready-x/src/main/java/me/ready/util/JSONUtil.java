e8951d3ecec0c2b90be5a716e3619a186a117d0d
CodePlayer <CodePlayer360@gmail.com>
2014-10-26 01:54:19 +0800

    添加JSONUtil工具类的编码和解码方法

--- ready-x/src/main/java/me/ready/util/JSONUtil.java
+++ ready-x/src/main/java/me/ready/util/JSONUtil.java
@@ -1,28 +1,51 @@
 package me.ready.util;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
  * JSON字符串转换工具类
+ * 
  * @package me.ready.util
  * @author Ready
  * @date 2014-10-13
- *
+ * 
  */
 public class JSONUtil {
 
 	// 禁止实例创建
-	private JSONUtil() {
-
-	}
+	private JSONUtil() {}
 
 	/**
 	 * 将Java对象编码为JSON字符串
+	 * 
 	 * @param obj
 	 * @return
 	 */
 	public static final String encode(Object obj) {
-		return JSONObject.valueToString(obj);
+		if (obj.getClass().isArray()) {
+			return new JSONArray(obj).toString();
+		}
+		return new JSONObject(obj).toString();
 	}
 
+	/**
+	 * 将JSON字符串转为JSONArray形式的数组(类似于增强型的ArrayList)
+	 * 
+	 * @param jsonStr
+	 * @return
+	 */
+	public static final JSONArray decodeForArray(String jsonStr) {
+		return new JSONArray(jsonStr);
+	}
+
+	/**
+	 * 将JSON字符串转为JSONObject形式的对象(类似于增强型的HashMap)
+	 * 
+	 * @param jsonStr
+	 * @return
+	 */
+	public static final JSONObject decodeForMap(String jsonStr) {
+		return new JSONObject(jsonStr);
+	}
 }
---
+++ ready-x/src/test/java/me/ready/util/JSONUtilTest.java
@@ -0,0 +1,38 @@
+package me.ready.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import me.ready.User;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class JSONUtilTest {
+
+	 @Test
+	public void testObject() {
+		User user = new User(1, "张三", "123456", true);
+		System.out.println(new JSONObject(user).toString());
+		System.err.println("====" + JSONUtil.encode(user));
+	}
+
+	// @Test
+	public void testMap() {
+		Map<Object, Object> map = CollectionUtil.createHashMap("name", "张三", "age", 18);
+		System.out.println(new JSONObject(map).toString());
+	}
+
+	// @Test
+	public void testArray() {
+		Object[] array = new Object[] { "张三", true, 15, new User(2, "李四", "45646", false) };
+		System.out.println(new JSONArray(array).toString());
+	}
+
+	@Test
+	public void testVar() {
+		String str = "张三";
+		System.out.println(new JSONObject(str).toString());
+	}
+}
