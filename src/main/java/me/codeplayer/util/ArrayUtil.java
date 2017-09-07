a6341fa0f28155f67e2fffd0acb94c509b078ac9
chinaq <chinaqsq@126.com>
2017-09-07 17:32:46 +0800

    修复缺少}的问题

--- src/main/java/me/codeplayer/util/ArrayUtil.java
+++ src/main/java/me/codeplayer/util/ArrayUtil.java
@@ -1,17 +1,15 @@
 package me.codeplayer.util;
 
-import java.lang.reflect.Array;
-import java.util.Collection;
-
 import me.codeplayer.e.LogicException;
 
 import java.lang.reflect.Array;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 用于对数组类型的数据(字节数组参见NumberUtil类)进行相应处理的工具类
- * 
+ *
  * @author Ready
  * @date 2012-9-29
  */
@@ -36,7 +34,7 @@ public abstract class ArrayUtil {
 
 	/**
 	 * 判断指定对象是否为数组类型
-	 * 
+	 *
 	 * @param obj 指定的对象
 	 * @return
 	 */
@@ -46,9 +44,9 @@ public abstract class ArrayUtil {
 
 	/**
 	 * 以指定的分隔符拼接数组元素，并追加到指定的StringBuilder中
-	 * 
-	 * @param sb 指定的StringBuilder（如果为null，则内部自动创建）
-	 * @param array 指定的数组
+	 *
+	 * @param sb        指定的StringBuilder（如果为null，则内部自动创建）
+	 * @param array     指定的数组
 	 * @param delimiter 指定的分隔符
 	 */
 	public static StringBuilder join(StringBuilder sb, Object array, String delimiter) {
@@ -73,8 +71,8 @@ public abstract class ArrayUtil {
 	/**
 	 * 以指定的分隔符拼接数组元素并返回拼接后的字符串<br>
 	 * 如果数组为空，将会引发异常
-	 * 
-	 * @param array 指定的数组对象
+	 *
+	 * @param array     指定的数组对象
 	 * @param delimiter 指定的分隔符
 	 * @return
 	 */
@@ -87,11 +85,11 @@ public abstract class ArrayUtil {
 	 * 如果数组为空，将会引发异常<br>
 	 * 如果数组元素只有一个，拼接内容为“=1”或“='1'”<br>
 	 * 如果数组元素有多个，拼接内容为“ IN (1, 2, 5)”或“ IN ('1', '2', '5')”
-	 * 
-	 * @param sb 指定的StringBuilder
-	 * @param array 指定的任意数组
+	 *
+	 * @param sb        指定的StringBuilder
+	 * @param array     指定的任意数组
 	 * @param isInclude 指示IN SQL是包含还是排除查询，如果是包含(true)将返回=、IN，如果是排除(false)将返回!=、NOT IN
-	 * @param isString 指示元素是否以字符串形式参与InSQL语句。如果为true，将会在每个元素两侧加上单引号"'"
+	 * @param isString  指示元素是否以字符串形式参与InSQL语句。如果为true，将会在每个元素两侧加上单引号"'"
 	 */
 	public static StringBuilder getInSQL(StringBuilder sb, Object array, boolean isInclude, boolean isString) {
 		if (array == null || !array.getClass().isArray()) {
@@ -102,29 +100,29 @@ public abstract class ArrayUtil {
 			sb = StringUtil.getBuilder(length, 3);
 		}
 		switch (length) {
-		case 0:
-			throw new LogicException("指定的数组对象的长度必须大于0!");
-		case 1:
-			if (!isInclude) {
-				sb.append('!');
-			}
-			if (isString) {
-				sb.append("='").append(Array.get(array, 0)).append('\'');
-			} else {
-				sb.append('=').append(Array.get(array, 0));
-			}
-			break;
-		default:
-			sb.append(isInclude ? " IN (" : " NOT IN (");
-			if (isString) {// 如果是字符串格式
-				sb.append('\'');
-				join(sb, array, "', '");
-				sb.append("')");
-			} else {// 如果是数字格式
-				join(sb, array, ", ");
-				sb.append(')');
-			}
-			break;
+			case 0:
+				throw new LogicException("指定的数组对象的长度必须大于0!");
+			case 1:
+				if (!isInclude) {
+					sb.append('!');
+				}
+				if (isString) {
+					sb.append("='").append(Array.get(array, 0)).append('\'');
+				} else {
+					sb.append('=').append(Array.get(array, 0));
+				}
+				break;
+			default:
+				sb.append(isInclude ? " IN (" : " NOT IN (");
+				if (isString) {// 如果是字符串格式
+					sb.append('\'');
+					join(sb, array, "', '");
+					sb.append("')");
+				} else {// 如果是数字格式
+					join(sb, array, ", ");
+					sb.append(')');
+				}
+				break;
 		}
 		return sb;
 	}
@@ -134,10 +132,10 @@ public abstract class ArrayUtil {
 	 * 如果数组为空，将会引发异常<br>
 	 * 如果数组元素只有一个，将会返回“=1”或“='1'”<br>
 	 * 如果数组元素有多个，将会返回“ IN (1, 2, 5)”或“ IN ('1', '2', '5')”
-	 * 
-	 * @param array 指定的数组
+	 *
+	 * @param array     指定的数组
 	 * @param isInclude 指示IN SQL是包含还是排除查询，如果是包含(true)将返回=、IN，如果是排除(false)将返回!=、NOT IN
-	 * @param isString 指示元素是否以字符串形式参与InSQL语句。如果为true，将会在每个元素两侧加上单引号"'"
+	 * @param isString  指示元素是否以字符串形式参与InSQL语句。如果为true，将会在每个元素两侧加上单引号"'"
 	 * @return
 	 */
 	public static String getInSQL(Object array, boolean isInclude, boolean isString) {
@@ -149,8 +147,8 @@ public abstract class ArrayUtil {
 	 * 如果数组为空，将会引发异常<br>
 	 * 如果数组元素只有一个，将会返回“=1”或“='1'”<br>
 	 * 如果数组元素有多个，将会返回“ IN (1, 2, 5)”或“ IN ('1', '2', '5')”
-	 * 
-	 * @param array 指定的数组
+	 *
+	 * @param array    指定的数组
 	 * @param isString 指示元素是否以字符串形式参与InSQL语句。如果为true，将会在每个元素两侧加上单引号"'"
 	 * @return
 	 */
@@ -162,7 +160,7 @@ public abstract class ArrayUtil {
 	 * 返回指定数组对象的字符串形式<br>
 	 * 如果<code>array</code>是一个数组，则迭代其元素返回字符串，如“[e1, e2, e3, e4]”<br>
 	 * 如果<code>array</code>不是一个数组，则直接调用String.valueOf()方法返回
-	 * 
+	 *
 	 * @param array
 	 * @return
 	 */
@@ -192,8 +190,8 @@ public abstract class ArrayUtil {
 	/**
 	 * 迭代数组元素并将迭代字符串追加至StringBuilder中,追加字符串形如：“[e1, e2, e3, [e4_1, e4_2, e4_3, e4_4]]<br>
 	 * 本方法可以迭代多维数组，内部采用递归算法
-	 * 
-	 * @param sb 指定的StringBuilder
+	 *
+	 * @param sb    指定的StringBuilder
 	 * @param array 指定的数组对象
 	 * @return
 	 */
@@ -229,7 +227,7 @@ public abstract class ArrayUtil {
 	/**
 	 * 迭代数组元素并返回迭代字符串，例如：“[e1, e2, e3, [e4_1, e4_2, e4_3, e4_4]]”<br>
 	 * 本方法可以迭代多维数组，内部采用递归算法
-	 * 
+	 *
 	 * @param array 指定的数组对象
 	 * @return
 	 */
@@ -240,7 +238,7 @@ public abstract class ArrayUtil {
 	/**
 	 * 判断指定数组是否不为null并且数组长度<code>length > 0</code>，如果是，则返回true<br>
 	 * 如果指定参数不为null，也不是数组类型，则引发异常
-	 * 
+	 *
 	 * @param array 指定的数组
 	 * @return
 	 */
@@ -250,8 +248,8 @@ public abstract class ArrayUtil {
 
 	/**
 	 * 获取指定数组元素的长度，如果数组为null将返回0，如果不是数组类型，将引发异常
-	 * 
-	 * @param array 指定的数组
+	 *
+	 * @param array        指定的数组
 	 * @param triggerError 当数组为null或数组长度为0时，是否触发异常，如果为true，则触发异常
 	 * @return
 	 */
@@ -266,7 +264,7 @@ public abstract class ArrayUtil {
 	/**
 	 * 获取指定数组元素的长度，如果指定的参数为null或长度为0，则返回0<br>
 	 * 如果指定参数不是数组类型，将引发异常
-	 * 
+	 *
 	 * @param array 指定的数组对象
 	 * @return
 	 */
@@ -281,7 +279,7 @@ public abstract class ArrayUtil {
 	 * 获取指定数组元素的"维度"，如果数组为普通的一维数组，则返回1；如果为二维数组，则返回2；以此类推...<br>
 	 * 如果不是数组，则返回-1<br>
 	 * <b>注意：</b>Java没有真正意义上的多维数组，只有嵌套数组
-	 * 
+	 *
 	 * @param array 指定的数组对象
 	 * @return
 	 * @author Ready
@@ -305,11 +303,11 @@ public abstract class ArrayUtil {
 
 	/**
 	 * 判断指定对象是否是一个原始类型的数组(例如：int[]、float[]、char[]、double[]等)
-	 * 
+	 *
 	 * @param array 指定的对象
 	 * @return
-	 * @since 0.3.1
 	 * @author Ready
+	 * @since 0.3.1
 	 */
 	public static final boolean isPrimitiveArray(Object array) {
 		if (array != null) {
@@ -324,12 +322,12 @@ public abstract class ArrayUtil {
 
 	/**
 	 * 检测指定的整数数组中是否存在指定的整数值
-	 * 
+	 *
 	 * @param value
 	 * @param array
 	 * @return
-	 * @since 0.3.5
 	 * @author Ready
+	 * @since 0.3.5
 	 */
 	public static final boolean in(int value, int... array) {
 		for (int i = 0; i < array.length; i++) {
@@ -342,12 +340,12 @@ public abstract class ArrayUtil {
 
 	/**
 	 * 检测指定的数组中是否存在指定的值
-	 * 
+	 *
 	 * @param value
 	 * @param array
 	 * @return
-	 * @since 0.3.5
 	 * @author Ready
+	 * @since 0.3.5
 	 */
 	@SafeVarargs
 	public static final <T> boolean ins(T value, T... array) {
@@ -397,6 +395,7 @@ public abstract class ArrayUtil {
 			return array;
 		}
 		return newArray;
+	}
 
 	@SuppressWarnings("unchecked")
 	public static final <T> T[] toArray(Collection<? extends T> collection, Class<T> type) {
