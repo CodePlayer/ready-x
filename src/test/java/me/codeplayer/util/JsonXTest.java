a56074e8a061f1a9bed12e9cec43b391cf6d2e63
CodePlayer <codeplayer360@gmail.com>
2025-02-08 20:24:25 +0800

    test(JsonX): 完善 JSON 相关测试用例

--- src/test/java/me/codeplayer/util/JsonXTest.java
+++ src/test/java/me/codeplayer/util/JsonXTest.java
@@ -2,8 +2,10 @@ package me.codeplayer.util;
 
 import java.util.*;
 
-import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.*;
 import org.assertj.core.api.WithAssertions;
+import org.assertj.core.data.Index;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class JsonXTest implements WithAssertions {
@@ -31,15 +33,95 @@ public class JsonXTest implements WithAssertions {
 				.containsEntry("name", "张三")
 				.containsEntry("age", 18);
 
-		assertThat(JsonX.encode(user)).isEqualTo("{\"gender\":true,\"id\":1,\"name\":\"张三\",\"password\":\"123456\"}");
+		jsonStr = JsonX.encode(user);
+		User parsed = JsonX.parseObject(jsonStr, User.class);
+		Assert.assertEquals(parsed, user);
 		// array
 		// ["Hello","CodePlayer",true,{"gender":true,"id":1,"name":"张三","password":"123456"}]
-		assertThat(JsonX.encode(array))
-				.isEqualTo("[\"Hello\",\"CodePlayer\",true,{\"gender\":true,\"id\":1,\"name\":\"张三\",\"password\":\"123456\"}]");
+		jsonStr = JsonX.encode(array);
+		JSONArray jsonArray = JsonX.parseArray(jsonStr);
+		assertThat(jsonArray)
+				.hasSize(4)
+				.contains("Hello", Index.atIndex(0))
+				.contains("CodePlayer", Index.atIndex(1))
+				.contains(true, Index.atIndex(2));
+		assertThat(jsonArray.getJSONObject(3))
+				.hasSize(5)
+				.containsEntry("id", 1)
+				.containsEntry("name", "张三")
+				.containsEntry("password", "123456")
+				.containsEntry("gender", true)
+				.containsEntry("admin", true);
 		// ArrayList
 		// 由于集合中存在重复的引用，请注意比较它与下面的encodeWithReferenceDetect()的输出区别
-		assertThat(JsonX.encode(users)) // [{"gender":true,"id":1,"name":"张三","password":"123456"},{"gender":true,"id":1,"name":"张三","password":"123456"}]
-				.isEqualTo("[{\"gender\":true,\"id\":1,\"name\":\"张三\",\"password\":\"123456\"},{\"gender\":true,\"id\":1,\"name\":\"张三\",\"password\":\"123456\"}]");
+		jsonStr = JsonX.encode(users);
+		List<User> list = JsonX.parseArray(jsonStr, User.class);
+		Assert.assertEquals(users, list);
+
+	}
+
+	@Test
+	public void encodeWithExclude() {
+		// map
+		String jsonStr = JsonX.encodeWithExclude(map, "age");
+		Assert.assertEquals("{\"name\":\"张三\"}", jsonStr);
+
+		jsonStr = JsonX.encodeWithExclude(user, "id", "password");
+		// POJO
+		LinkedHashMap<String, Object> json = JsonX.parseObject(jsonStr, new TypeReference<LinkedHashMap<String, Object>>() {
+		});
+		assertThat(json)
+				.hasSize(3)
+				.containsEntry("name", "张三")
+				.containsEntry("admin", true)
+				.containsEntry("gender", true);
+	}
+
+	@Test
+	public void encodeWithInclude() {
+		// map
+		String jsonStr = JsonX.encodeWithInclude(map, "name");
+		Assert.assertEquals("{\"name\":\"张三\"}", jsonStr);
+
+		jsonStr = JsonX.encodeWithInclude(user, "id", "name");
+		// POJO
+		JSONObject json = (JSONObject) JsonX.parse(jsonStr);
+		assertThat(json)
+				.hasSize(2)
+				.containsEntry("name", "张三")
+				.containsEntry("id", 1);
+	}
+
+	@Test
+	public void encodeRaw() {
+		// map
+		String jsonStr = JsonX.encodeRaw(map);
+		assertThat(jsonStr).isEqualTo("{\"name\":\"张三\",\"age\":18}");
+
+		jsonStr = JsonX.encodeRaw(user);
+		User actual = JsonX.parseObject(jsonStr, User.class);
+		Assert.assertEquals(user, actual);
+		// array
+		// ["Hello","CodePlayer",true,{"gender":true,"id":1,"name":"张三","password":"123456"}]
+		jsonStr = JsonX.encodeRaw(array);
+		JSONArray jsonArray = JsonX.parseArray(jsonStr);
+		assertThat(jsonArray)
+				.hasSize(4)
+				.contains("Hello", Index.atIndex(0))
+				.contains("CodePlayer", Index.atIndex(1))
+				.contains(true, Index.atIndex(2));
+		assertThat(jsonArray.getJSONObject(3))
+				.hasSize(5)
+				.containsEntry("id", 1)
+				.containsEntry("name", "张三")
+				.containsEntry("password", "123456")
+				.containsEntry("gender", true)
+				.containsEntry("admin", true);
+		// ArrayList
+		// 由于集合中存在重复的引用，请注意比较它与下面的encodeWithReferenceDetect()的输出区别
+		jsonStr = JsonX.encodeRaw(users);
+		List<User> list = JsonX.parseArray(jsonStr, User.class);
+		Assert.assertEquals(users, list);
 
 	}
 
@@ -54,14 +136,14 @@ public class JsonXTest implements WithAssertions {
 		String text = "{\"gender\":true,\"id\":1,\"name\":\"张三\",\"password\":\"123456\"}";
 		User user = JsonX.parseObject(text, User.class);
 		assertThat(user)
-				.extracting("name", "password", "id", "gender")
-				.containsExactly("张三", "123456", 1, true);
+				.extracting("name", "password", "id", "gender", "admin")
+				.containsExactly("张三", "123456", 1, true, true);
 
 		text = "{\"name\":\"张三\",\"password\":\"123456\"}";
 		user = JsonX.parseObject(text, User.class);
 		assertThat(user)
 				.extracting("name", "password", "id", "gender")
-				.containsExactly("张三", "123456", 0, false);
+				.containsExactly("张三", "123456", null, false);
 	}
 
 	@Test
@@ -74,7 +156,7 @@ public class JsonXTest implements WithAssertions {
 
 	static class User {
 
-		private int id;
+		private Integer id;
 		private String name;
 		private String password;
 		private boolean gender;
@@ -89,11 +171,11 @@ public class JsonXTest implements WithAssertions {
 			this.gender = gender;
 		}
 
-		public int getId() {
+		public Integer getId() {
 			return id;
 		}
 
-		public void setId(int id) {
+		public void setId(Integer id) {
 			this.id = id;
 		}
 
@@ -119,6 +201,27 @@ public class JsonXTest implements WithAssertions {
 
 		public void setGender(boolean gender) {
 			this.gender = gender;
+		}
+
+		public boolean isAdmin() {
+			return Cmp.eq(id, 1);
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
+			User user = (User) o;
+			return gender == user.gender && Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(password, user.password);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id, name, password, gender);
 		}
 
 	}
