113933325f3214de85037dadece9c1b7df0987b4
CodePlayer <codeplayer360@gmail.com>
2025-02-13 14:18:35 +0800

    test: 适配 JDK 9+ 的日期格式变化
    - 修改了 EasyDateTest 中的测试用例，以适应 JDK 9+ 中月份表示的变化
    - 使用 ArrayX.ins 方法替代 assertEquals，以支持阿拉伯数字和中文月份的兼容性

--- src/test/java/me/codeplayer/util/EasyDateTest.java
+++ src/test/java/me/codeplayer/util/EasyDateTest.java
@@ -89,8 +89,10 @@ public class EasyDateTest implements WithAssertions {
 
 		final Locale defLocale = Locale.getDefault();
 		Locale.setDefault(Locale.CHINA);
-		assertEquals("星期四, 28 二月 2013 23:59:59 GMT", d.toGMTNetString());
-		assertEquals("14 二月 2009 07:31:30 GMT", EasyDate.valueOf(baseDate).toGMTString());
+
+		// JDK 9+ 的月份采用的是阿拉伯数字
+		assertTrue(ArrayX.ins(d.toGMTNetString(), "星期四, 28 二月 2013 23:59:59 GMT", "星期四, 28 2月 2013 23:59:59 GMT"));
+		assertTrue(ArrayX.ins(EasyDate.valueOf(baseDate).toGMTString(), "14 二月 2009 07:31:30 GMT", "14 2月 2009 07:31:30 GMT"));
 		Locale.setDefault(defLocale);
 
 		assertEquals("20130205", EasyDate.toShortString(date));
