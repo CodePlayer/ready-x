982a22f531954178663d5d50753cb5a0a6641ef9
CodePlayer <codeplayer360@gmail.com>
2025-08-20 10:38:57 +0800

    test: 为 JavaTest 类添加静态方法模拟单元测试

--- src/test/java/me/codeplayer/util/JavaTest.java
+++ src/test/java/me/codeplayer/util/JavaTest.java
@@ -1,6 +1,11 @@
 package me.codeplayer.util;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JavaTest {
 
@@ -11,6 +16,16 @@ public class JavaTest {
 		File dest = new File("C:\\Users\\pc\\Desktop\\Eclipse-target-properties.png");
 		FileUtil.moveFile(src, dest, true);
 		*/
+	}
+
+	@Test
+	public void mock() {
+		try (MockedStatic<StringUtil> mocked = Mockito.mockStatic(StringUtil.class)) {
+			mocked.when(() -> StringUtil.isEmpty("hello")).thenReturn(true);
+
+			assertTrue(StringUtil.isEmpty("hello"));
+			assertFalse(StringUtil.isEmpty("world"));
+		}
 	}
 
 }
